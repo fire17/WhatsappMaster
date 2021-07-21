bd45b343ebc71752382f27c45a90b3ed83fee0f8
Tami Bar <fire17@gmail.com>
2021-07-21 04:36:47 +0300

    sandbox

--- app.py
+++ app.py
@@ -224,7 +224,7 @@ class Master(object):
 				# chrome_options.add_argument('headless')
 				chrome_options.add_argument("--headless")
 				chrome_options.add_argument("--disable-dev-shm-usage")
-				# chrome_options.add_argument("--no-sandbox")
+				chrome_options.add_argument("--no-sandbox")
 				# chrome_options.add_argument("--window-size=1420,3600")
 			chrome_options.add_argument("--user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36")
 			# user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'
